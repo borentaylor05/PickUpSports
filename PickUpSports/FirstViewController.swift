fa037c3aa9cfa9d856a41502330403231f80a555
Taylor Boren <taylorboren@teletech.com>
2015-07-06 15:33:14 -0600

    Initial Commit

---
+++ PickUpSports/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PickUpSports
+//
+//  Created by Taylor Boren on 7/6/15.
+//  Copyright (c) 2015 Taylor Boren. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PickUpSports/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PickUpSports
+//
+//  Created by Taylor Boren on 7/6/15.
+//  Copyright (c) 2015 Taylor Boren. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
